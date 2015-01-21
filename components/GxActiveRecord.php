ed7a2990831f016a5122d2c8dcd9f9fa4414dac7
Jiří Svoboda <jiri.svoboda@dlds.cz>
2015-01-21 13:30:15 +0100

    GxActiveRecord featurs

--- components/GxActiveRecord.php
+++ components/GxActiveRecord.php
@@ -8,6 +8,8 @@
 
 namespace dlds\giixer\components;
 
+use yii\helpers\ArrayHelper;
+use yii\helpers\StringHelper;
 use yii\db\ActiveRecord;
 
 /**
@@ -26,6 +28,60 @@ class GxActiveRecord extends ActiveRecord {
         return (string) $this->representingColumn();
     }
 
+    /**
+     * Loads given parameters
+     * @param array $params
+     */
+    public function load($data, $formName = null)
+    {
+        if ($data)
+        {
+            $className = StringHelper::basename(static::className());
+
+            foreach ($data as $key => $value)
+            {
+                if (preg_match(sprintf('/^%s_.*$/', $className), $key))
+                {
+                    $attr = ltrim(strstr($key, '_'), '_');
+
+                    $data[$className][$attr] = $value;
+
+                    unset($data[$key]);
+                }
+            }
+        }
+
+        return parent::load($data, $formName);
+    }
+
+    /**
+     * Removes validation rule from given rules
+     * @param array $rules
+     */
+    protected function removeValidationRules(&$rules, $validator, array $attrs = [])
+    {
+        foreach ($rules as $i => &$rule)
+        {
+            if ($validator === $rule[1])
+            {
+                if (empty($attrs))
+                {
+                    ArrayHelper::remove($rules, $i);
+                }
+                else
+                {
+                    foreach ($rule[0] as $j => $attr)
+                    {
+                        if (in_array($attr, $attrs))
+                        {
+                            ArrayHelper::remove($rule[0], $j);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
     /**
      * Retrieves model representing column
      */
