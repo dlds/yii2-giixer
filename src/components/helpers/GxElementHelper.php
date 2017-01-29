b9bf57a53c24d0df8725fdce0675449bb5a48b58
Jiří Svoboda <svobik7@gmail.com>
2017-01-30 00:18:54 +0100

    + GxElementHelper ids

--- src/components/helpers/GxElementHelper.php
+++ src/components/helpers/GxElementHelper.php
@@ -34,4 +34,43 @@ class GxElementHelper
         return $id;
     }
 
+    /**
+     * Retrieves form id
+     * @param string $id
+     * @param boolean $withHash
+     * @param string $suffix
+     */
+    public static function idForm($id, $withHash = true, $suffix = null)
+    {
+        $suffix = trim(sprintf('%s-wrapper', $suffix), '-');
+
+        return static::id($id, $withHash, $suffix);
+    }
+
+    /**
+     * Retrieves pager id
+     * @param string $id
+     * @param boolean $withHash
+     * @param string $suffix
+     */
+    public static function idPager($id, $withHash = true, $suffix = null)
+    {
+        $suffix = trim(sprintf('%s-pager', $suffix), '-');
+
+        return static::id($id, $withHash, $suffix);
+    }
+
+    /**
+     * Retrieves wrapper id
+     * @param string $id
+     * @param boolean $withHash
+     * @param string $suffix
+     */
+    public static function idWrapper($id, $withHash = true, $suffix = null)
+    {
+        $suffix = trim(sprintf('%s-wrapper', $suffix), '-');
+
+        return static::id($id, $withHash, $suffix);
+    }
+
 }
